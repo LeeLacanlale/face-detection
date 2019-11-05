57c7fd41b6ce1afb0fc9547e99e4421ab3c9d2f2
Lee Lacanlale <lee.lacanlale@gmail.com>
2019-11-05 13:56:39 -0800

    added route code

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,11 +1,20 @@
 import React from 'react';
 
-const Navigation = () => {
-  return (
-    <nav className='right'>
-      <p className='f3 link dim black underline pa3 pointer'>Sign Out</p>
-    </nav>
-  )
+const Navigation = ({ routeChange, isSignedIn }) => {
+  if (isSignedIn) {
+    return (
+      <nav className='right'>
+        <p onClick={() => routeChange('signin')} className='f3 link dim black underline pa3 pointer'>Sign Out</p>
+      </nav>
+    );
+  } else {
+    return (
+      <nav className='right'>
+        <p onClick={() => routeChange('signin')} className='f3 link dim black underline pa3 pointer'>Sign In</p>
+        <p onClick={() => routeChange('register')} className='f3 link dim black underline pa3 pointer'>Register</p>
+      </nav>
+    );
+  }
 }
 
 export default Navigation;
